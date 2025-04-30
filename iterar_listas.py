34f409a56789103974e73322847877dc5fb0abcf
Victor_Carmona <victor.carmona@grupoica.com>
2025-04-30 20:50:56 +0200

    Iterar elementos de una lista

---
+++ iterar_listas.py
@@ -0,0 +1,13 @@
+print(f'*** Iterar listas ***')
+
+nombres = ['Karla', 'Juan', 'Laura']
+
+for nombre in nombres:
+    print(nombre)
+
+print(f'<----------------------------->')
+
+lista_heterogenea = [100, True, 'Ivonne']
+
+for elemento in lista_heterogenea:
+    print(elemento)
