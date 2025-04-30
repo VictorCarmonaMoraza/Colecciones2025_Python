7485950f09530578fbfd40f7472c95a775d7822f
Victor_Carmona <victor.carmona@grupoica.com>
2025-04-30 19:28:34 +0200

    Ejemplo de listas en python

---
+++ listas.py
@@ -0,0 +1,14 @@
+print('***Manejo de Listas***')
+
+mi_lista = [1, 2, 3, 4, 5]
+
+print(f'{mi_lista} -> Lista original')
+
+# Largo de la lista
+print(f'El largo de la lista es: {len(mi_lista)}')
+
+# Acceder a los elementos de la lista por indice
+print(f'Accedemos al valor del indice 2: {mi_lista[2]}')
+
+# Accedemos al ultimo indice de la lista
+print(f'Accedemos al ultimo inidce de la lista {mi_lista[-1]}')
